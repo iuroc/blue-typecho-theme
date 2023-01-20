b489180823fda167d48163eee390c10f25419198
oyps <ponconsoft@gmail.com>
2023-01-20 21:39:57 +0800

    ..

--- header.php
+++ header.php
@@ -15,17 +15,7 @@
             ], '', ' - ');
             $this->options->title(); ?></title>
     <link rel="stylesheet" href="https://cdn.staticfile.org/github-markdown-css/5.1.0/github-markdown-dark.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/oyps/blue-typecho-theme/style.css">
-    <style>
-        @font-face {
-            font-family: "apee";
-            src: url(https://cdn.jsdelivr.net/gh/oyps/blue-typecho-theme/TsangerYuYangT_W03_W03.woff);
-        }
-
-        * {
-            font-family: 'apee';
-        }
-    </style>
+    <link rel="stylesheet" href="<?php $this->options->themeUrl('style.css'); ?>">
     <script>
         window.onload = function() {
             document.body.ondragstart = () => {
